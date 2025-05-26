73f5627f6fe37aae66fa109e782b7e7eac1467b7
Antonio Augusto D Agostino <antonio.etec.inf5@gmail.com>
2025-05-26 07:39:29 -0300

    Remove extra WordPress configuration file: delete config.extra.php to streamline setup

--- config/wp/config.extra.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('FS_METHOD', 'direct');
-define('WORDPRESS_DEBUG', true);
-define('WORDPRESS_DEBUG_LOG', true);
-define('WORDPRESS_DEBUG_DISPLAY', false);
-define( 'WP_MEMORY_LIMIT', '128M' ); 
-define( 'WP_MAX_MEMORY_LIMIT', '512M' );
